8b05041d134be7b0bcab0e0c4a3141c0cd0f59cd
a.berman <sr.Lux1nt@gmail.com>
2019-06-22 18:15:42 +0300

    add conditional compiling in component.mk file

--- component.mk
+++ component.mk
@@ -1,3 +1,4 @@
 # Component Makefile
 #
 # (Uses default behaviour of compiling all source files in directory, adding 'include' to include path.)
+$(call compile_only_if,$(CONFIG_MAGICSEE_R1_CONTROLLER),r1.o)
